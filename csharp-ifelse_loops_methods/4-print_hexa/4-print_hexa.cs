b244c6c15a07a894524f031071eb9dd49033cdbe
TonyMeme <tonychong203@gmail.com>
2023-07-24 23:03:30 +0200

    Print number from 0 to 98 in decimal and hexadecimal

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+        
+            while (i <= 98)
+            {
+                Console.Write("{0} = 0x{0:X}\n", i, i);
+                i++;
+            }
+        }
+    }
+}
