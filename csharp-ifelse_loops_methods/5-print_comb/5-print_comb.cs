439b652c84730d7c4e198c961e29ac8070ff9fc6
TonyMeme <tonychong203@gmail.com>
2023-07-24 23:16:53 +0200

    Print number from 00 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+        
+            for (i = 0; i < 100; i++)
+            {
+                Console.Write("{0:D2}", i);
+                if (i < 99)
+                Console.Write(", ");
+            }
+        }
+    }
+}
