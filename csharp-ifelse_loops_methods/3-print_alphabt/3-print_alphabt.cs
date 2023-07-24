4305164950f89910be82be619232a78f9feb9612
TonyMeme <tonychong203@gmail.com>
2023-07-24 22:49:32 +0200

    Print alphabet in lowercase except q and e

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabt;
+        
+            for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+            {
+                if (alphabt != 'q' && alphabt != 'e')
+                {
+                    Console.Write(alphabt);
+                }
+            }
+        }
+    }
+}
