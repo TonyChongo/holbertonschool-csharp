e3ee5c59b9719018de29dd4e09fb18ad7cc658a8
TonyMeme <tonychong203@gmail.com>
2023-07-24 15:22:57 +0200

    C# Script that prints an interger as a variable follow by a text

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -2,12 +2,9 @@
 
 namespace _4_print_integer
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
             int number = 972;
             Console.WriteLine("{0} Mission Street", number);
-        }
     }
 }
