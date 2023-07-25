25596be59ca419033f29a3a019882f0652d7ada7
TonyMeme <tonychong203@gmail.com>
2023-07-25 18:35:46 +0200

    Print a script to standard error;

---
+++ csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string errorMessage = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+        Console.Error.WriteLine(errorMessage);
+        Environment.Exit(1);
+    }
+}
