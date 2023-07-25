c3b869827c7a727fba6b8ed6628e3e4d91ea763f
TonyMeme <tonychong203@gmail.com>
2023-07-25 18:16:07 +0200

    draws a diagonal line

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(new string(' ', i));
+                Console.WriteLine("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
