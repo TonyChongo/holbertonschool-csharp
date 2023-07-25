6781cb447e4d5ad3fb30a25cfb5f9ec1c357c050
TonyMeme <tonychong203@gmail.com>
2023-07-25 11:00:38 +0200

    Print all possible difference combination of two digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+
+            for (first = 0; first < 9; first++)
+            {
+                for (second = first + 1; second <= 9; second++)
+                {
+                    Console.Write("{0}{1}", first, second);
+                    if (first < 8 || second < 9)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
