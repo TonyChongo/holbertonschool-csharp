12223854e4b68ac67d3b2f4d0c3cc907867c6b2d
TonyMeme <tonychong203@gmail.com>
2023-07-31 00:09:56 +0200

    Print an array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(newArray[i] + " ");
+        }
+        Console.WriteLine();
+
+        return newArray;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
+    }
+}
