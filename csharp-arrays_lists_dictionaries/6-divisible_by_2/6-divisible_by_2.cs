876051e30b310893315cd13f42d1e143deb6aea4
TonyMeme <tonychong203@gmail.com>
2023-07-31 11:12:56 +0200

    Find all interger that multiples with 2

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>(myList.Count);
+
+        foreach (int number in myList)
+        {
+            bool isDivisionBy2 = (number % 2 == 0);
+            result.Add(isDivisionBy2);
+        }
+
+        return result;
+    }
+}
