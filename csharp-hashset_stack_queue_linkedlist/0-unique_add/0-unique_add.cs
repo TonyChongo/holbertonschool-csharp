87ff490bd0ab265b439c795d2e9dbbdd3ecd9aa7
TonyMeme <tonychong203@gmail.com>
2023-07-31 19:50:44 +0200

    Add all unique integers in a list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        Dictionary<int, int> uniqueInteger = new Dictionary<int, int>();
+
+        foreach (int num in myList)
+        {
+            if (uniqueInteger.ContainsKey(num))
+            {
+                uniqueInteger[num]++;
+            }
+            else
+            {
+                uniqueInteger[num] = 1;
+            }
+        }
+        int sum = 0;
+        foreach (var kvp in uniqueInteger)
+        {
+            sum += kvp.Key;
+        }
+        return sum;
+    }
+}
