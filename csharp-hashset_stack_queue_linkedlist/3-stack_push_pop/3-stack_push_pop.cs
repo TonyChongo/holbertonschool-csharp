8e933e6a64cceb3118a7081451bc441e4be271aa
TonyMeme <tonychong203@gmail.com>
2023-07-31 22:55:54 +0200

    Write a full stack method

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+        if (aStack.Count > 0)
+            Console.WriteLine("Top item: " + aStack.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains " + search + ": " + containsSearch);
+
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            while (aStack.Peek() != search)
+            {
+                tempStack.Push(aStack.Pop());
+            }
+            aStack.Pop(); // Remove the search item
+            while (tempStack.Count > 0)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
