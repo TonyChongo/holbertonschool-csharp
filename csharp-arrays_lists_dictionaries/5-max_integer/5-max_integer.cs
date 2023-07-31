cc112f81d87b5fcac3fa47d8c3bc2e1b2640cb1a
TonyMeme <tonychong203@gmail.com>
2023-07-31 11:04:34 +0200

    Find the biggest of a list of interger

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int maxint = myList[0];
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] > maxint)
+            {
+                maxint = myList[i];
+            }
+        }
+        return maxint;
+    }
+}
