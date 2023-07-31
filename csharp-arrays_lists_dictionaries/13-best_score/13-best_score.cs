5ec92b7e362978c7b2cfaf48749e7d4eb77a2b77
TonyMeme <tonychong203@gmail.com>
2023-07-31 15:27:05 +0200

    Find the key with the biggest integer value in a given dictionary

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int MaxScore = -1;
+        string bestKey = "None";
+
+        foreach (var kvp in myList)
+        {
+            if (kvp.Value > MaxScore)
+            {
+                MaxScore = kvp.Value;
+                bestKey = kvp.Key;
+            }
+        }
+        return bestKey;
+    }
+}
