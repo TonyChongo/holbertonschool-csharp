d2bf69928ac0c2b61d1c5a7d280bf77d20f835cf
TonyMeme <tonychong203@gmail.com>
2023-07-31 20:11:53 +0200

    Find common elements in two lists

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        Dictionary<int, int> elementCount = new Dictionary<int, int>();
+
+        foreach (int num in list1)
+        {
+            if (elementCount.ContainsKey(num))
+            {
+                elementCount[num]++;
+            }
+            else
+            {
+                elementCount[num] = 1;
+            }
+        }
+
+        List<int> commonElement = new List<int>();
+
+        foreach (int num in list2)
+        {
+            if (elementCount.TryGetValue(num, out int count) && count > 0)
+            {
+                commonElement.Add(num);
+                elementCount[num]--;
+            }
+        }
+
+        commonElement.Sort();
+
+        return commonElement;
+    }
+}
