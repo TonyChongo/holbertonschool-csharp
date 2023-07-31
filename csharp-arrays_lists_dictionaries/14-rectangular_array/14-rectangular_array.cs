c10008e2d6128cc6cf2c5de6a34ebb4f780c1c96
TonyMeme <tonychong203@gmail.com>
2023-07-31 15:38:57 +0200

    Create and print a rectangle array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                array[i, j] = 0;
+            }
+        }
+
+        array[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
