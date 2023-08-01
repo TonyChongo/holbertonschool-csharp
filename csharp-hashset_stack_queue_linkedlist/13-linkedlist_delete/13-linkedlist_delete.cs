057e3639e34d5cb023244d538c39a19988a91189
TonyMeme <tonychong203@gmail.com>
2023-08-01 11:15:05 +0200

    Delete a node in likelist

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,11 +5,6 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (index < 0 || index >= myLList.Count)
-        {
-            throw new ArgumentOutOfRangeException("index", "Invalid index position");
-        }
-
         LinkedListNode<int> currentNode = myLList.First;
         for (int i = 0; i < index; i++)
         {
