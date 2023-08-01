f0a6b8e2454a6ba95883f627a47c45b394bb7e67
TonyMeme <tonychong203@gmail.com>
2023-08-01 11:01:49 +0200

    Get the value of the nth node of a LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n < 0 || n >= myLList.Count)
+        {
+            return 0;
+        }
+
+        LinkedListNode<int> currentNode = myLList.First;
+        for (int i = 0; i < n; i++)
+        {
+            currentNode = currentNode.Next;
+        }
+        return currentNode.Value;
+    }
+}
