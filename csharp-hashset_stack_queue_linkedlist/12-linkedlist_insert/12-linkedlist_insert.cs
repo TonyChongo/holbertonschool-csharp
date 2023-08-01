3beb76e2f0806cf172a841d97b91e4e5cdb22140
TonyMeme <tonychong203@gmail.com>
2023-08-01 11:09:27 +0200

    inserts a new node in the correct position in an ordered LinkedList.

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+
+        if (myLList.Count == 0 || n < myLList.First.Value)
+        {
+            myLList.AddFirst(newNode);
+            return newNode;
+        }
+        LinkedListNode<int> current = myLList.First;
+        while (current.Next != null)
+        {
+            if (n >= current.Value && n <= current.Next.Value)
+            {
+                myLList.AddAfter(current, newNode);
+                return newNode;
+            }
+            current = current.Next;
+        }
+        myLList.AddLast(newNode);
+        return newNode;
+    }
+}
