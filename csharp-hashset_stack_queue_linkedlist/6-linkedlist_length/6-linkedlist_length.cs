a58c71ac8f14bae7f380756e461c036a8a1140f3
TonyMeme <tonychong203@gmail.com>
2023-08-01 10:44:53 +0200

    Print the number of elements in a LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+
+        if (myLList != null)
+        {
+            LinkedListNode<int> currentNode = myLList.First;
+            while (currentNode != null)
+            {
+                count++;
+                currentNode = currentNode.Next;
+            }
+        }
+        return count;
+    }
+}
