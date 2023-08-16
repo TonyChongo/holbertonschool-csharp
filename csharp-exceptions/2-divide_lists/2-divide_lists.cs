8cb144d1c9a3a149d0c5ae5c6f0dee9296b2e801
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-16 09:18:31 +0200

    Divide 2 lists

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> resultList = new List<int>();
+        
+
+        for (int i = 0; i < listLength; i++)
+        {
+            int result = 0;
+            try
+            {
+                result = list1[i] / list2[i];
+                resultList.Add(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                resultList.Add(result);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return resultList;
+    }
+}
