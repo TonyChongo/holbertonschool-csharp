e56471dca4aae668e4c1b87547401e21baeaa08b
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-21 10:28:33 +0200

    Create new public class that contains public field

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class Zombie that contains their health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Public field
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
+
