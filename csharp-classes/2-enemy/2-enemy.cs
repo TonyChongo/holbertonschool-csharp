46d54008b3dd727f6c4390ede9505992a63ed5fb
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-21 10:42:08 +0200

    Insert new public constructor

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class Zombie that contains their health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Public field
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// New public contructor
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
+
