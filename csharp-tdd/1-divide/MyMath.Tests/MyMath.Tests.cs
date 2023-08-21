e4dc06dec008b40084997795e7ef10102a500060
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-21 18:43:27 +0200

    Add test cases

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,61 @@
 using NUnit.Framework;
+using MyMath; // Make sure to adjust the namespace if necessary
 
-namespace Tests
+namespace MyMathTests
 {
-    public class Tests
+    [TestFixture]
+    public class MatrixTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Divide_ValidInput_ReturnsDividedMatrix()
         {
+            // Arrange
+            int[,] inputMatrix = {
+                { 10, 20 },
+                { 30, 40 }
+            };
+            int num = 2;
+            int[,] expectedOutput = {
+                { 5, 10 },
+                { 15, 20 }
+            };
+
+            // Act
+            int[,] result = Matrix.Divide(inputMatrix, num);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedOutput));
         }
 
         [Test]
-        public void Test1()
+        public void Divide_NullMatrix_ReturnsNull()
         {
-            Assert.Pass();
+            // Arrange
+            int[,] inputMatrix = null;
+            int num = 2;
+
+            // Act
+            int[,] result = Matrix.Divide(inputMatrix, num);
+
+            // Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Divide_DivideByZero_ReturnsNull()
+        {
+            // Arrange
+            int[,] inputMatrix = {
+                { 10, 20 },
+                { 30, 40 }
+            };
+            int num = 0;
+
+            // Act
+            int[,] result = Matrix.Divide(inputMatrix, num);
+
+            // Assert
+            Assert.That(result, Is.Null);
         }
     }
 }
