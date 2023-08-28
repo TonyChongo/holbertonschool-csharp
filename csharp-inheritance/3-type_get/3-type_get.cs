a7dc9edcc8688bb5debf7ce5577efff208c0455c
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 09:57:03 +0200

    Print the names of the available properties and methods of an object

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// Method class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method prototype
+    /// </summary>
+    /// <param name="myObj"></param>
+    public static void Print(object myObj)
+    {
+        Type objType = myObj.GetType();
+
+        Console.WriteLine("{0} Properties:", objType.Name);
+        foreach (PropertyInfo property in objType.GetProperties())
+        {
+            Console.WriteLine("{0}", property.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", objType.Name);
+        foreach (MethodInfo method in objType.GetMethods())
+        {
+            Console.WriteLine("{0}", method.Name);
+        } 
+    }
+}
