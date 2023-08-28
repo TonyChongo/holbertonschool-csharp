6ca8e3b2cfd5fb3084b43114195a2d6a836e1ebd
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 11:01:29 +0200

    Add method that defines full square

---
+++ csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+/// <summary>
+/// Class name
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Public method
+    /// </summary>
+    /// <returns></returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Subclass rectangle that inherits from class Shape
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Public property of width
+    /// </summary>
+    /// <value></value>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Public property of height
+    /// </summary>
+    /// <value></value>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+
+    /// <summary>
+    /// Public property of area
+    /// </summary>
+    /// <returns></returns>
+    public new int Area()
+    {
+        return Width * Height;
+    }
+
+    /// <summary>
+    /// Public property that devide rectangle
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+}
+
+/// <summary>
+/// New class square that inherits from class rectangle
+/// </summary>
+class Square : Rectangle
+{
+    private int size;
+
+    /// <summary>
+    /// New public property
+    /// </summary>
+    /// <value></value>
+    public int Size
+    {
+        get { return size; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            size = value;
+            Width = value;
+            Height = value;
+        }
+    }
+
+    /// <summary>
+    /// Public property that devide square
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"[Square] {size} / {size}";
+    }
+}
