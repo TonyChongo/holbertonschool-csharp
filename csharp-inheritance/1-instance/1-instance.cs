92258f672a9f100a78f6b8f837941bfeaf01984b
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 09:40:29 +0200

    Verify if object is Array

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Method class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Prototype of method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
