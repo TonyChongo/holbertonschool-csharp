bd1d13d266e4d65b09904685c9ab763f5a8af2f7
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 10:09:07 +0200

    Throw an exception error in a class

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Class name
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Public method
+    /// </summary>
+    /// <returns></returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
