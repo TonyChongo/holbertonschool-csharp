ee51574f231308e3b831ec6202d32f1b0165342e
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 09:45:50 +0200

    Check  if the object is an instance of a class that inherits from the specified class

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Class method
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Prototype of method
+    /// </summary>
+    /// <param name="derivedType"></param>
+    /// <param name="baseType"></param>
+    /// <returns></returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return baseType.IsAssignableFrom(derivedType) && derivedType != baseType;
+    }
+}
