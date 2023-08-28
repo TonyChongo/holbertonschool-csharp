76951856ee8a2fb2cc9063acd2b19538260bdcae
ChongLeangUENG <leangcnrp777@gmail.com>
2023-08-28 09:31:25 +0200

    Add XML comments

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Class of method
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// Prototype of method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int;
