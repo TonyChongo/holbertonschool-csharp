339bfd468c56233fa18bb076874c31e4a18b59e7
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-12 22:48:23 +0200

    calculates the determinant of a matrix

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,39 +10,30 @@ public class MatrixMath
     /// </summary>
     /// <param name="matrix"></param>
     /// <returns></returns>
-    public static double Determinant(double[,] matrix, int decimalPlaces = 2)
+    public static double Determinant(double[,] matrix)
     {
-        int numRows = matrix.GetLength(0);
-        int numCols = matrix.GetLength(1);
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
 
-        // Check if the matrix is not 2D or 3D
-        if (numRows != numCols || (numRows != 2 && numRows != 3))
+        if (rows != cols || (rows != 2 && rows != 3))
         {
-            // Return -1 for invalid dimensions
+            // Return -1 for matrices that are not 2x2 or 3x3
             return -1;
         }
 
-        double determinant = 0;
-
-        if (numRows == 2)
+        if (rows == 2)
         {
-            // For a 2x2 matrix, calculate the determinant directly
-            determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            // For a 2x2 matrix
+            double det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            return Math.Round(det, 2);
         }
-        else if (numRows == 3)
+        else
         {
-            // For a 3x3 matrix, use the formula to calculate the determinant
-            determinant = (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
-                          (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) +
-                          (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]) -
-                          (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) -
-                          (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) -
-                          (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
+            // For a 3x3 matrix
+            double det = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1])
+                         - matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0])
+                         + matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+            return Math.Round(det, 2);
         }
-
-        // Round the determinant to the specified number of decimal places
-        determinant = Math.Round(determinant, decimalPlaces);
-
-        return determinant;
     }
 }
