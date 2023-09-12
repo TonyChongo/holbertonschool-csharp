179f9bf6d754d2de6cf5205231988a0e118af392
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-12 23:19:12 +0200

    calculates the inverse of a 2D matrix and returns the resulting matrix

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Public class
+/// </summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Public static
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != 2 || cols != 2)
+        {
+            // Return [-1] for non-2D matrices
+            return new double[,] { { -1 } };
+        }
+
+        double determinant = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+
+        if (Math.Abs(determinant) < 1e-6)
+        {
+            // Matrix is non-invertible (determinant is close to zero)
+            return new double[,] { { -1 } };
+        }
+
+        double[,] inverse = new double[2, 2];
+
+        inverse[0, 0] = matrix[1, 1] / determinant;
+        inverse[0, 1] = -matrix[0, 1] / determinant;
+        inverse[1, 0] = -matrix[1, 0] / determinant;
+        inverse[1, 1] = matrix[0, 0] / determinant;
+
+        return inverse;
+    }
+}
