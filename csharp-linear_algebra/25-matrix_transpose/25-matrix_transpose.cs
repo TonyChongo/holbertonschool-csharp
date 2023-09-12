2a50ec032ee6773d20c3e4621f75b1e3671a2e5b
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-12 22:11:05 +0200

    transpose a matrix and return the resulting matrix

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Public class
+/// </summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Public static
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int numRows = matrix.GetLength(0);
+        int numCols = matrix.GetLength(1);
+
+        // Check if the matrix is empty
+        if (numRows == 0 || numCols == 0)
+        {
+            // Return an empty matrix if it's empty
+            return new double[0, 0];
+        }
+
+        // Create a new matrix to store the transposed values
+        double[,] resultMatrix = new double[numCols, numRows];
+
+        // Perform the transpose by swapping rows and columns
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                resultMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return resultMatrix;
+    }
+}
