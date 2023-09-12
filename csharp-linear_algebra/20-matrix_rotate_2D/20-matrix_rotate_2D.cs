052e566bbbeaca942a2d7cf888456aa3db1bd068
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-12 10:25:43 +0200

    rotates a square 2D matrix by a given angle in radians and returns the resulting matrix

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Public class
+/// </summary>
+public static class MatrixMath
+{
+    /// <summary>
+    /// Public static
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        // Ensure it's a square matrix
+        if (rows != cols)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] rotatedMatrix = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                double x = matrix[i, j];
+                double y = matrix[j, i];
+
+                // Apply rotation
+                rotatedMatrix[i, j] = x * Math.Cos(angle) - y * Math.Sin(angle);
+            }
+        }
+
+        return rotatedMatrix;
+    }
+}
