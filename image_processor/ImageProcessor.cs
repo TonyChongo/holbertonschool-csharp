28bc236fc2cf34ad5ed9343b67eb30a25c3168c8
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-25 15:38:14 +0200

    Inverse image

---
+++ image_processor/ImageProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+class ImageProcessor
+{
+    /// <summary>
+    /// Public static
+    /// </summary>
+    /// <param name="filenames"></param>
+    public static void Inverse(string[] filenames)
+    {
+        foreach (var filename in filenames)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    // Load the original image
+                    using (var image = new Bitmap(filename))
+                    {
+                        // Create a new image with the same dimensions
+                        using (var newImage = new Bitmap(image.Width, image.Height))
+                        {
+                            for (int x = 0; x < image.Width; x++)
+                            {
+                                for (int y = 0; y < image.Height; y++)
+                                {
+                                    // Get the original pixel color
+                                    Color originalColor = image.GetPixel(x, y);
+
+                                    // Invert the color
+                                    Color invertedColor = Color.FromArgb(
+                                        255 - originalColor.R,
+                                        255 - originalColor.G,
+                                        255 - originalColor.B
+                                    );
+
+                                    // Set the inverted color in the new image
+                                    newImage.SetPixel(x, y, invertedColor);
+                                }
+                            }
+
+                            // Determine the output filename
+                            string outputFilename = Path.GetFileNameWithoutExtension(filename)
+                                                    + "_inverse" + Path.GetExtension(filename);
+
+                            // Save the new image with the same format as the original
+                            newImage.Save(outputFilename, image.RawFormat);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing {filename}: {ex.Message}");
+                }
+            }).Wait(); // Wait for each task to complete
+        }
+    }
+}
---
+++ image_processor/README.md
@@ -0,0 +1,12 @@
+Image Processing
+
+Parallel Processing, Concurrency, and Async Programming in .NET
+Using threads and threading
+System.Drawing Namespace
+For this project you will be utilizing .NET’s System.Drawing namespace. You will need to install it using NuGet.
+
+Mac users: In addition to adding the System.Drawing NuGet package, you will need to also do the following:
+
+brew install mono-libgdiplus
+In the project folder: dotnet add package runtime.osx.10.10-x64.CoreCompat.System.Drawing
+All images in this project are sourced from Unsplash.
