c110365ac5416b242b97092934b1ac864b4d77ee
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-27 10:57:45 +0200

    Add a new method to create a thumnail

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -186,25 +186,10 @@ public class ImageProcessor
         return bwData;
     }
 
-    private static int CalculateThumbnailWidth(string filename, int targetHeight)
+    private static int CalculateThumbnailWidth(int originalWidth, int originalHeight, int targetHeight)
     {
-        try
-        {
-            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                byte[] header = new byte[24];
-                fs.Read(header, 0, 24);
-                int width = header[12] + (header[13] << 8) + (header[14] << 16) + (header[15] << 24);
-                int height = header[16] + (header[17] << 8) + (header[18] << 16) + (header[19] << 24);
-                double aspectRatio = (double)width / height;
-                return (int)(targetHeight * aspectRatio);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error while calculating thumbnail width for {filename}: {ex.Message}");
-            return -1;
-        }
+        double aspectRatio = (double)originalWidth / originalHeight;
+        return (int)(targetHeight * aspectRatio);
     }
 
     private static byte[] CreateThumbnail(byte[] imageData, int width, int height)
