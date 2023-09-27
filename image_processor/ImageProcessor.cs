74658d8b49926e9fc24dbf2a23e141f557063b05
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-27 09:39:01 +0200

    Add a new method that converts each image to grayscale

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -33,6 +33,33 @@ public class ImageProcessor
         }
     }
 
+    /// <summary>
+    ///  public static void Grayscale
+    /// </summary>
+    /// <param name="filenames"></param>
+    public static void Grayscale(string[] filenames)
+    {
+        foreach (string filename in filenames)
+        {
+            try
+            {
+                // Read the original image as a byte array
+                byte[] imageData = File.ReadAllBytes(filename);
+
+                // Convert the image to grayscale
+                byte[] grayscaleData = ConvertToGrayscale(imageData);
+
+                // Save the grayscale image with the "_grayscale" suffix
+                string outputFilename = $"{GetFileNameWithoutExtension(filename)}_grayscale{GetFileExtension(filename)}";
+                File.WriteAllBytes(outputFilename, grayscaleData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing {filename}: {ex.Message}");
+            }
+        }
+    }
+
     private static byte[] InvertColors(byte[] imageData)
     {
         byte[] invertedData = new byte[imageData.Length];
@@ -45,6 +72,23 @@ public class ImageProcessor
 
         return invertedData;
     }
+
+    private static byte[] ConvertToGrayscale(byte[] imageData)
+    {
+        byte[] grayscaleData = new byte[imageData.Length];
+
+        for (int i = 0; i < imageData.Length; i += 3)
+        {
+            // Calculate grayscale value for each pixel using the luminance formula
+            byte pixelValue = (byte)(0.299 * imageData[i] + 0.587 * imageData[i + 1] + 0.114 * imageData[i + 2]);
+
+            // Set the same grayscale value for all three color channels
+            grayscaleData[i] = grayscaleData[i + 1] = grayscaleData[i + 2] = pixelValue;
+        }
+
+        return grayscaleData;
+    }
+
 
     private static string GetFileNameWithoutExtension(string filename)
     {
