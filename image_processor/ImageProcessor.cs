c0989b3416c31de4ca87a22769af0e59b317e755
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-27 09:33:55 +0200

    Fix fonction not work

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,60 +1,58 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Parallel;
 
-class ImageProcessor
+/// <summary>
+/// public class ImageProcessor
+/// </summary>
+public class ImageProcessor
 {
     /// <summary>
-    /// public static
+    /// public static void Inverse
     /// </summary>
     /// <param name="filenames"></param>
     public static void Inverse(string[] filenames)
     {
-        Parallel.ForEach(filenames, (filename) =>
+        foreach (string filename in filenames)
         {
             try
             {
-                // Load the original image
-                using (var image = new Bitmap(filename))
-                {
-                    // Create a new image with the same dimensions
-                    using (var newImage = new Bitmap(image.Width, image.Height))
-                    {
-                        for (int x = 0; x < image.Width; x++)
-                        {
-                            for (int y = 0; y < image.Height; y++)
-                            {
-                                // Get the original pixel color
-                                Color originalColor = image.GetPixel(x, y);
+                // Read the original image as a byte array
+                byte[] imageData = File.ReadAllBytes(filename);
 
-                                // Invert the color
-                                Color invertedColor = Color.FromArgb(
-                                    255 - originalColor.R,
-                                    255 - originalColor.G,
-                                    255 - originalColor.B
-                                );
+                // Invert the colors of the image
+                byte[] invertedData = InvertColors(imageData);
 
-                                // Set the inverted color in the new image
-                                newImage.SetPixel(x, y, invertedColor);
-                            }
-                        }
-
-                        // Determine the output filename
-                        string outputFilename = Path.GetFileNameWithoutExtension(filename)
-                                                + "_inverse" + Path.GetExtension(filename);
-
-                        // Save the new image with the same format as the original
-                        newImage.Save(outputFilename, image.RawFormat);
-                    }
-                }
+                // Save the inverted image with the "_inverse" suffix
+                string outputFilename = $"{GetFileNameWithoutExtension(filename)}_inverse{GetFileExtension(filename)}";
+                File.WriteAllBytes(outputFilename, invertedData);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error processing {filename}: {ex.Message}");
             }
-        });
+        }
+    }
+
+    private static byte[] InvertColors(byte[] imageData)
+    {
+        byte[] invertedData = new byte[imageData.Length];
+
+        for (int i = 0; i < imageData.Length; i++)
+        {
+            // Invert each byte (color channel)
+            invertedData[i] = (byte)(255 - imageData[i]);
+        }
+
+        return invertedData;
+    }
+
+    private static string GetFileNameWithoutExtension(string filename)
+    {
+        return Path.GetFileNameWithoutExtension(filename);
+    }
+
+    private static string GetFileExtension(string filename)
+    {
+        return Path.GetExtension(filename);
     }
 }
