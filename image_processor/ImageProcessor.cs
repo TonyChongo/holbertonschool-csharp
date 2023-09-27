832ff645999c01f0f0320a2224c7ea156d0fb2e6
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-27 09:47:46 +0200

    Add a new method to reduce image BW

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -60,6 +60,34 @@ public class ImageProcessor
         }
     }
 
+    /// <summary>
+    /// public static void BlackWhite
+    /// </summary>
+    /// <param name="filenames"></param>
+    /// <param name="threshold"></param>
+    public static void BlackWhite(string[] filenames, double threshold)
+    {
+        foreach (string filename in filenames)
+        {
+            try
+            {
+                // Read the original image as a byte array
+                byte[] imageData = File.ReadAllBytes(filename);
+
+                // Convert the image to black and white based on the threshold
+                byte[] blackWhiteData = ConvertToBlackWhite(imageData, threshold);
+
+                // Save the black and white image with the "_bw" suffix
+                string outputFilename = $"{GetFileNameWithoutExtension(filename)}_bw{GetFileExtension(filename)}";
+                File.WriteAllBytes(outputFilename, blackWhiteData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing {filename}: {ex.Message}");
+            }
+        }
+    }
+
     private static byte[] InvertColors(byte[] imageData)
     {
         byte[] invertedData = new byte[imageData.Length];
@@ -93,6 +121,25 @@ public class ImageProcessor
         }
 
         return grayscaleData;
+    }
+
+    private static byte[] ConvertToBlackWhite(byte[] imageData, double threshold)
+    {
+        byte[] bwData = new byte[imageData.Length];
+
+        for (int i = 0; i < imageData.Length; i += 3)
+        {
+            // Calculate grayscale value for each pixel using the luminance formula
+            double luminance = 0.299 * imageData[i] + 0.587 * imageData[i + 1] + 0.114 * imageData[i + 2];
+
+            // Determine whether the pixel should be black or white based on the threshold
+            byte pixelValue = (luminance >= threshold) ? (byte)255 : (byte)0;
+
+            // Set the same black or white value for all three color channels
+            bwData[i] = bwData[i + 1] = bwData[i + 2] = pixelValue;
+        }
+
+        return bwData;
     }
 
 
