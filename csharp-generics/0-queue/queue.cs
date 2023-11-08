86ea4438f2e18fb9bacaff7373aab98ba1c7c7cb
ChongLeangUENG <leangcnrp777@gmail.com>
2023-11-08 10:47:48 +0100

    Create a new generic class and method

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Public generic class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Public generic method that returns the Queue’s type
+    /// </summary>
+    /// <returns></returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+}
