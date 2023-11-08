de0960ce0ff79e10bb38c16cb4be08f053982230
ChongLeangUENG <leangcnrp777@gmail.com>
2023-11-08 17:25:10 +0100

    Add a new class name Base of Abstract thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// An abstract class called Base
+/// </summary>
+public abstract class  Base
+{
+    /// <summary>
+    /// public property name
+    /// </summary>
+    /// <value></value>
+    public string name { get; set; }
+
+    /// <summary>
+    /// override ToString() method
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
