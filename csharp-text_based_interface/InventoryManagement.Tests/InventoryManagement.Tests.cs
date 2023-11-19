71cf23416b51d6442f5a4c13f01968b961238712
ChongLeangUENG <leangcnrp777@gmail.com>
2023-11-19 17:34:37 +0100

    Add unittest

---
+++ csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventoryLibrary;
+
+[TestClass]
+public class JSONStorageTests
+{
+    [TestMethod]
+    public void TestNewObject()
+    {
+        // Arrange
+        JSONStorage storage = new JSONStorage();
+        Item item = new Item
+        {
+            id = "item1",
+            name = "Test Item",
+            description = "Test description",
+            price = 10.99f,
+            tags = new List<string> { "tag1", "tag2" }
+        };
+
+        // Act
+        storage.New(item);
+
+        // Assert
+        Assert.IsTrue(storage.All().ContainsKey("Item.item1"));
+    }
+
+    // More test methods for other JSONStorage functionality...
+}
